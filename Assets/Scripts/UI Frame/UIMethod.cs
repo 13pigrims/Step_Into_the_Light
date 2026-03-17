6b58a236e0036f8c8d02037758230a5ea60764b6
13pigrims <145422720+13pigrims@users.noreply.github.com>
2026-03-17 12:27:01 +0800

    Update UIMethod.cs

--- Assets/Scripts/UI Frame/UIMethod.cs
+++ Assets/Scripts/UI Frame/UIMethod.cs
@@ -21,7 +21,7 @@ public class UIMethod
         }
     }
     /// <summary>
-    /// 寻找场景中的Canvas物体（注：可能会遇到多个Canvas情况，需根据优先级进行判断，此处代码待优化）
+    /// 寻找场景中的Canvas对象（注：可能会遇到多个Canvas情况，需根据优先级进行判断，此处代码待优化）
     /// </summary>
     /// <returns></returns>
     public GameObject FindCanvas()
@@ -36,7 +36,7 @@ public class UIMethod
          
     }
     /// <summary>
-    /// 寻找Panel物体下的子物体
+    /// 寻找Panel物体下的子对象
     /// </summary>
     /// <param name="parent_Panel"></param>
     /// <param name="child_name"></param>
@@ -54,14 +54,43 @@ public class UIMethod
         Debug.LogError("未在Panel中找到子物体");
         return null;
     }
+    /// <summary>
+    /// 在目标对象上寻找组件（注：缺少添加组件的功能，待优化）
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="Get_Obj"></param>
+    /// <returns></returns>
 
-    public GameObject AddOrGetComponent()
+    public T AddOrGetComponent<T>(GameObject Get_Obj) where T : Component
     {
+        if (Get_Obj.GetComponent<T>() != null)
+        {
+            return Get_Obj.GetComponent<T>();
+        }
+        Debug.LogError("未在目标对象上找到组件");
         return null;
     }
-
-    public GameObject GetOrAddSingleComponentInChild()
+    /// <summary>
+    /// 获得目标对象上的子物体上的组件（注：可能会遇到多个同名子物体情况，需根据优先级进行判断，此处代码待优化），同时缺少添加组件的功能，待优化
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="panel"></param>
+    /// <param name="ComponentName"></param>
+    /// <returns></returns>
+    public T GetOrAddSingleComponentInChild<T>(GameObject panel, string ComponentName) where T : Component
     {
+        Transform[] transforms = panel.GetComponentsInChildren<Transform>();
+        foreach (var t in transforms)
+        {
+            if (t.gameObject.name == ComponentName)
+            {
+                if (t.GetComponent<T>() != null)
+                {
+                    return t.gameObject.GetComponent<T>();
+                }
+            }
+        }
+        Debug.LogError("未在Panel中找到子物体");
         return null;
     }
 
